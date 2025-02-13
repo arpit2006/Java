6f9298f940a1166a31c1001dac3e0c1c9e8c041a
arpit2006 <arpitshirbhate25@gmail.com>
2025-02-13 23:46:53 +0530

    initial comit

---
+++ Arrays/Array_1.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class Array_1 {
+    public static void main(String[] args) {
+        //Arrays id used to store multiple type of data
+        //Arrays is collection of similar datatypes
+        //Syntax -->
+        // datatype of array --> name of array --> = [];
+
+        //Declaration and memory allocation
+        //int [] marks =new int [5];
+
+        //First declaration and then allocation
+//        int [] marks;
+//        marks = new int[5];
+//        marks[0] = 100; //position of elements
+//        marks[1] = 10;
+//        marks[2] = 90;
+//        marks[3] = 80;
+//        marks[4] = 20;
+//        System.out.println(marks[0]);
+
+        //all methos together
+        int [] marks = {10,20,30,40,50};
+        System.out.println(marks[2]);
+
+    }
+}
